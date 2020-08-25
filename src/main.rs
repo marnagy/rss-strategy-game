869bc213acff704dcf7f0fef58e14d906a638355
Marek Nagy <62247071@cuni.cz>
2020-08-25 16:30:32 +0200

    added structs Page, Move

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ extern crate num;
 #[macro_use]
 extern crate num_derive;
 
-use std::io;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader, BufWriter};
 
 enum ResourceType {
     Gold(u32),
-    Wood(u32)
+    Wood(u32),
 }
 
 enum Buildings {
-    Farm
+    Farm,
 }
 
 enum GameAction {
@@ -29,7 +31,7 @@ struct GameState {
     current_turn: i32,
     end_turn: i32,
     resources: Resources,
-    buildings: Vec::<Buildings>
+    buildings: Vec<Buildings>,
 }
 
 impl GameState {
@@ -38,7 +40,7 @@ impl GameState {
             current_turn: 0,
             end_turn,
             resources: Resources { gold: 0, wood: 0 },
-            buildings: Vec::new()
+            buildings: Vec::new(),
         }
     }
 
@@ -49,6 +51,8 @@ impl GameState {
             let action: GameAction = get_user_action();
 
             self.do_action(action);
+
+            // clear screen
 
             self.current_turn += 1;
         }
@@ -64,7 +68,7 @@ impl GameState {
                     };
                     self.do_build_action(Buildings::Farm);
                 }
-            },
+            }
             GameAction::Harvest(resource) => self.do_harvest_action(resource),
             _ => println!("Action not implemented"),
         }
@@ -77,7 +81,7 @@ impl GameState {
     fn do_harvest_action(&mut self, resources: ResourceType) {
         match resources {
             ResourceType::Gold(gold) => self.resources.gold += gold,
-            ResourceType::Wood(wood) => self.resources.wood += wood
+            ResourceType::Wood(wood) => self.resources.wood += wood,
         }
 
         println!("Your resources: {:?}", self.resources);
@@ -86,52 +90,123 @@ impl GameState {
 
 fn get_user_action() -> GameAction {
     loop {
-        println!("Choose your action:");
-        println!("1: Build");
-        println!("2: Harvest");
+        // println!("Choose your action:");
+        // println!("1: Build");
+        // println!("2: Harvest");
 
-        let mut action: String = String::new();
-        io::stdin().read_line(&mut action).unwrap();
+        // let mut action: String = String::new();
+        // stdin().read_line(&mut action).unwrap();
 
-        let action_result = action.trim().parse::<i32>();
-        match action_result {
-            Ok(act) => {
-                match act {
-                    1 => return GameAction::Build,
-                    2 => return GameAction::Harvest(get_harvest_action()),
-                    3 => return GameAction::Pass,
-                    _ => continue
-                }
-            }
-            Err(_) => continue,
-        }
+        // let action_result = action.trim().parse::<i32>();
+        // match action_result {
+        //     Ok(act) => match act {
+        //         1 => return GameAction::Build,
+        //         2 => return GameAction::Harvest(get_harvest_action()),
+        //         3 => return GameAction::Pass,
+        //         _ => continue,
+        //     },
+        //     Err(_) => continue,
+        // }
     }
 }
 
-fn get_harvest_action() -> ResourceType  {
-    loop {
-        println!("What do you want to harvest:");
-        println!("1: Gold");
-        println!("2: Wood");
+// fn get_harvest_action() -> ResourceType {
+//     loop {
+//         println!("What do you want to harvest:");
+//         println!("1: Gold");
+//         println!("2: Wood");
 
-        let mut action: String = String::new();
-        io::stdin().read_line(&mut action).unwrap();
+//         let mut action: String = String::new();
+//         stdin().read_line(&mut action).unwrap();
 
-        let action_result = action.trim().parse::<i32>();
-        match action_result {
-            Ok(act) => {
-                match act {
-                    1 => return ResourceType::Gold(500),
-                    2 => return ResourceType::Wood(500),
-                    _ => continue
-                }
+//         let action_result = action.trim().parse::<i32>();
+//         match action_result {
+//             Ok(act) => match act {
+//                 1 => return ResourceType::Gold(500),
+//                 2 => return ResourceType::Wood(500),
+//                 _ => continue,
+//             },
+//             Err(_) => continue,
+//         }
+//     }
+// }
+
+struct Page {
+    Number: i32,
+    Final: bool,
+    Text: String,
+    Moves: Vec<Move>,
+    Success: bool
+}
+struct Move {
+    next_page: i32,
+    text: String
+}
+
+impl Page {
+    fn from(file: File) -> HashMap<i32, Page> {
+        let mut pages = HashMap::new();
+        let mut moves = Vec::new();
+        let mut buf = HashMap::new();
+        let reader = BufReader::new(file);
+
+        for line in reader.lines().map(|l| l.unwrap()) {
+            println!("Line -> {}", line);
+            if line.contains(":") && line.contains(";"){
+                let parts: Vec<&str> = line.split(';').collect();
+                let parts0 : Vec<&str> = parts[0].split(":").collect();
+                let parts1 : Vec<&str> = parts[1].split(":").collect();
+                moves.push( Move {
+                    next_page: parts0[1].parse().unwrap(),
+                    text: String::from(parts1[1])
+                } )
             }
-            Err(_) => continue,
+            else if line.contains(":") {
+                let parts: Vec<&str> = line.split(':').collect();
+                let part0 = String::from(parts[0]);
+                let part1 = String::from(parts[1]);
+                println!("Line parts: 0 -> {0}, 1 -> {1}", part0, part1);
+                buf.insert( part0, part1 );
+            }
+
+            if line == "---" {
+                let final_attr = buf.get("Final").unwrap();
+                let number_attr = buf.get("Page").unwrap().parse().unwrap();
+                let text_attr = String::from( buf.get("Text").unwrap() );
+                if final_attr == "Y" {
+                    let success_attr: bool = buf.get("Success").unwrap().parse::<bool>().unwrap();
+
+                    pages.insert( number_attr, Page {
+                        Number: number_attr,
+                        Final: true,
+                        Text: text_attr,
+                        Moves: Vec::new(),
+                        Success: success_attr
+                    });
+                }
+                else if final_attr == "N" {
+                    pages.insert( number_attr, Page {
+                        Number: number_attr,
+                        Final: true,
+                        Text: text_attr,
+                        Moves: moves,
+                        Success: false
+                    });
+                    moves = Vec::new();
+                }
+                else{
+                    panic!("Invalid format of pages.");
+                }
+                buf.clear();
+            }
         }
+
+        pages
     }
 }
 
 fn main() {
-    let mut game = GameState::new(10);
+    let pages = Page::from( File::open( String::from("pages.txt")).unwrap() );
+    let mut game = GameState::new( pages.len() as i32);
     game.game_loop();
 }
